086f0f80540af52e9bc5fc3cb89e66aa6501aa94
B2MW <P5@Champion.local>
2014-10-09 18:14:44 -0500

    Initial Commit

---
+++ LionsAndTigers2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers2
+//
+//  Created by Bradley Walker on 10/9/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LionsAndTigers2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LionsAndTigers2
+//
+//  Created by Bradley Walker on 10/9/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
